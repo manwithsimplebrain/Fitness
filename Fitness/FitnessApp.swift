f71d2f0db001904d7d94809e1dcc4324ee183fa5
manwithsimplebrain <manwithsimplebrain@gmail.com>
2025-02-27 21:10:55 +0700

    Initial Commit

---
+++ Fitness/FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessApp.swift
+//  Fitness
+//
+//  Created by Dat Doan on 27/2/25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitnessTests/FitnessTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTests.swift
+//  FitnessTests
+//
+//  Created by Dat Doan on 27/2/25.
+//
+
+import Testing
+@testable import Fitness
+
+struct FitnessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
